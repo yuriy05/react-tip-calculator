9e5735f329c4ac307e60bb5c719d635ce108e540
yuriy05 <dilayyura@gmail.com>
2024-08-22 23:34:23 +0300

    react tip calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [price, setPrice] = useState(null);
+  const [tip, setTip] = useState({yourTip: 5, friendTip: 5});
+
+  const yourTip = tip.yourTip;
+  const friendTip = tip.friendTip;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Bill onSetPrice={setPrice} />
+      <SelectPercentage text="How did you like the service?" onSetTip={(value) => setTip({...tip, yourTip: value})} tip={yourTip}/>
+      <SelectPercentage text="How did your friend like the service?" onSetTip={(value) => setTip({...tip, friendTip: value})} tip={friendTip}/>
+      <Output price={price} yourTip={tip.yourTip} friendTip={tip.friendTip} />
+    </>
   );
+}
+
+function Bill({onSetPrice}) {
+  return (
+    <div style={{display: 'flex', gap: 10, alignItems: 'center'}}>
+      <p>How much was the bill?</p>
+      <input type="number" onChange={(e) => onSetPrice(Number(e.target.value))} />
+    </div>
+  )
+}
+
+function SelectPercentage({text, onSetTip, tip}) {
+  return (
+    <div style={{display: 'flex', gap: 10, alignItems: 'center'}}>
+    <p>{text}</p>
+    <select value={tip} onChange={(e) => onSetTip(Number(e.target.value))}>
+      <option value='0'>Disssatisfied (0%)</option>
+      <option value='5'>It was okay (5%)</option>
+      <option value='10'>It was good (10%)</option>
+      <option value='20'>Absolutely Amazing (20%)</option>
+    </select>
+  </div>
+  )
+}
+
+function Output({price, yourTip, friendTip}) {
+  const tip = price * ((yourTip + friendTip) / 2 / 100);
+
+  return (
+    <div>
+      {price > 0 && (<p>You pay ${price + tip}  (${price} + ${tip} tip)</p>)}
+    </div>
+  )
 }
 
 export default App;
